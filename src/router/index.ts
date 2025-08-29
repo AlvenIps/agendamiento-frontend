22abeb01706f638ae56dc60fd1d02277320b453c
j0se25 <andresquinon25@gmail.com>
2025-08-29 01:10:10 -0500

    redirect uri login

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ const router = createRouter({
       component: AgendarCitaView,
     },
     {
-      path: '/login',
+      path: '/signin',
       name: 'login',
       component: LoginView,
     },
